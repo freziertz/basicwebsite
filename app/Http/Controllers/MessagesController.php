ae0ca7ca625936e0546da18ff0900e8811e1d490
frezier <freziern@gmail.com>
2018-09-05 12:09:46 +0300

    Initial commit

---
+++ app/Http/Controllers/MessagesController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers;
+
+
+use Illuminate\Http\Request;
+use App\Message;
+
+class MessagesController extends Controller
+{
+    public function submit(Request $request){
+      $this->validate($request, [
+        'name'=>'required',
+        'email'=> 'required'
+      ]);
+
+      //Create messages
+      $message = new Message;
+      $message->name = $request->input('name');
+      $message->email = $request->input('email');
+      $message->message = $request->input('message');
+
+      //Save messages
+      $message->save();
+
+      //Redirect
+      return redirect('/')->with('success','message sent');
+
+    }
+
+    public function getMessages(){
+      $messages = Message::all();
+
+      return view('messages')->with('messages', $messages);
+    }
+}
---
+++ resources/views/inc/sidebar.blade.php
@@ -0,0 +1,7 @@
+@section('sidebar')
+<div class="well">
+  <h3>Sidebar</h3>
+  <p>This is the sidebar</p>
+  </div>
+@show {{--You must add that to show appended content defined on the page
+  look at home page--}}
